96d197e046105a9519f75bf2367dd4842810a391
ansonfoong <afoong@albany.edu>
2019-07-04 11:33:11 -0400

    Prompt user for api key.

--- src/weather/clj.clj
+++
@@ -1,6 +0,0 @@
-(ns weather.clj)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- src/weather/main.clj
+++ src/weather/main.clj
@@ -1,4 +1,7 @@
 (ns weather.main)
 
-(defn -main [& args] 
-  (println "Hello World!"))
+(defn -main [& args]
+  (println "Please enter your API key from openweathermap.org")
+  (let [api-key (read-line)]
+    (println (str "Your API key: " api-key)))
+)
