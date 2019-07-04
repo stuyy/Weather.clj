9550bcd125d37bd17bf9009e1b72c19580c1f3b0
ansonfoong <afoong@albany.edu>
2019-07-04 13:29:52 -0400

    Loop until user provides valid key or exit. Make http call

--- src/weather/main.clj
+++ src/weather/main.clj
@@ -6,12 +6,13 @@
   '("current" "city-name" "gets the current forecast for given city")
   '("forecast" "city-name" "gets the forecast for given city")])
 
-(defn validate-key [endpoint-url]
-  (println (str "Making HTTP/GET request to " endpoint-url))
-  (let [url endpoint-url]
-    (def response (client/get url {:async? true}
-      (fn [res] (println res))
-      (fn [ex] (println "exception: " (.getMessage ex)))))))
+(defn validate-key [url]
+  (println (str "Making HTTP/GET request to " url))
+  (try 
+    (let [res (client/get url)]
+      (:body res))
+    (catch Exception err
+      (println (.getMessage err)) nil)))
 
 (defn display-menu []
   (println "Enter a command followed by its parameters to get started!")
@@ -19,15 +20,25 @@
   (loop [m menu]
     (if (empty? m) nil (do (println (first m)) (recur (rest m))))))
 
+(defn commands []
+  (display-menu) ; display menu for user.
+  (println "Enter a command")
+  (loop [cmd (read-line)]
+    (if (= cmd "exit") (println "Goodbye!") (do (println "Enter a command") (recur (read-line))))))
+
 (defn -main [& args]
-  (println "Please enter your API key from openweathermap.org")
   ; make http get request to validate their key.
-  (let [api-key (read-line)]
-    (validate-key (str base-url api-key))
-    (println (str "Your API key: " api-key))
-    (display-menu) ; display menu for user.
-    
-    (loop [cmd (read-line)]
-      (if (= cmd "exit") (println "Goodbye!") (recur (read-line)))
-    )
-))
+  (println "Please enter your API key from openweathermap.org")
+  (loop [api-key (read-line)]
+    (if (= api-key "exit") 
+      (println "Goodbye!")
+      (do
+        (let [res (validate-key (str base-url api-key))]
+          (if (not= res nil) 
+            (do
+              (println "Valid API KEY!")
+              (commands))
+            (do 
+              (println "Invalid API Key. Try again?") 
+              (recur (read-line)))
+))))))
